c9902cf5ef649585fded657848883179a1b81977
Suraj Pulloor <thegamer600@gmail.com>
2019-02-05 15:40:42 +0530

    Added tests for multiplication, and updated toBe() values for new inputs

--- test/equ-equ-bigint.test.js
+++ test/equ-equ-bigint.test.js
@@ -4,13 +4,13 @@ let bI = require('../dist/bigint');
 // let num2 = new bI.BigInt('152365214589963258896547125332658965874521452236589895648899856325412154788598521455589632511145785963254111852395475632115698547253622229999998547856923316589');
 
 // test variables
-let num1 = new bI.BigInt('24136');
-let num2 = new bI.BigInt('24136');
+let num1 = new bI.BigInt('2413641');
+let num2 = new bI.BigInt('2413641');
 
 
 // Test #1
 test('Adding num1 + num2', () => {
-    expect(num1.add(num2).toString()).toBe('48272');
+    expect(num1.add(num2).toString()).toBe('4827282');
 });
 
 // Test #2
@@ -23,10 +23,10 @@ test('Reverse Subtracting num2 - num1', () => {
     expect(num2.subtract(num1).toString()).toBe('0');
 });
 
-// // Test #4
-// test('Multiplying num1 * num2', () => {
-//     expect(num1.multiply(num2).toString()).toBe('1000000');
-// });
+// Test #4
+test('Multiplying num1 * num2', () => {
+    expect(num1.multiply(num2).toString()).toBe('5825662876881');
+});
 
 // Test #5
 test('LessThan num1 < num2', () => {
--- test/neg-neg-bigint.test.js
+++ test/neg-neg-bigint.test.js
@@ -4,48 +4,48 @@ let bI = require('../dist/bigint');
 // let num2 = new bI.BigInt('152365214589963258896547125332658965874521452236589895648899856325412154788598521455589632511145785963254111852395475632115698547253622229999998547856923316589');
 
 // test variables
-let num1 = new bI.BigInt('-26325');
-let num2 = new bI.BigInt('-44125253');
+let num1 = new bI.BigInt('-2552359');
+let num2 = new bI.BigInt('-2538458');
 
 
 // Test #1
 test('Adding num1 + num2', () => {
-    expect(num1.add(num2).toString()).toBe('-44151578');
+    expect(num1.add(num2).toString()).toBe('-5090817');
 });
 
 // Test #2
 test('Subtracting num1 - num2', () => {
-    expect(num1.subtract(num2).toString()).toBe('44098928');
+    expect(num1.subtract(num2).toString()).toBe('-13901');
 });
 
 // Test #3
 test('Reverse Subtracting num2 - num1', () => {
-    expect(num2.subtract(num1).toString()).toBe('-44098928');
+    expect(num2.subtract(num1).toString()).toBe('13901');
 });
 
-// // Test #4
-// test('Multiplying num1 * num2', () => {
-//     expect(num1.multiply(num2).toString()).toBe('1000000000000');
-// });
+// Test #4
+test('Multiplying num1 * num2', () => {
+    expect(num1.multiply(num2).toString()).toBe('6479056122422');
+});
 
 // Test #5
 test('LessThan num1 < num2', () => {
-    expect(num1.lessThan(num2)).toBe(false);
+    expect(num1.lessThan(num2)).toBe(true);
 });
 
 // Test #6
 test('LessThanEqual num1 <= num2', () => {
-    expect(num1.lessThanEqual(num2)).toBe(false);
+    expect(num1.lessThanEqual(num2)).toBe(true);
 });
 
 // Test #7
 test('GreaterThan num1 > num2', () => {
-    expect(num1.greaterThan(num2)).toBe(true);
+    expect(num1.greaterThan(num2)).toBe(false);
 });
 
 // Test #8
 test('GreaterThanEqual num1 >= num2', () => {
-    expect(num1.greaterThanEqual(num2)).toBe(true);
+    expect(num1.greaterThanEqual(num2)).toBe(false);
 });
 
 // Test #9
