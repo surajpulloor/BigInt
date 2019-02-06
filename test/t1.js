fef85a637bbde1763fe0ddcde5af2f2b5057b216
Suraj Pulloor <thegamer600@gmail.com>
2019-02-06 15:22:56 +0530

    Added loop based factorial function, Added timing code to evaluate the performance of the function for larger number

--- test/t1.js
+++ test/t1.js
@@ -3,8 +3,10 @@ let bI = require('../dist/bigint');
 // let num1 = new bI.BigInt('504153625965821445899632548000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005125874693251452158896325412589652365226574125552669854125669852314582148547965231552463258965741253996587452145236589657441253265896352333365215245555896574555632663215898778541256985485');
 // let num2 = new bI.BigInt('152365214589963258896547125332658965874521452236589895648899856325412154788598521455589632511145785963254111852395475632115698547253622229999998547856923316589412536254859658745231526985478512532658957458125325612547778569852142562258745886963254125874589996325412555233221245899557452114523666578845212365478889652114523365289574125632554125577');
 
-let num1 = new bI.BigInt('200');
+let num1 = new bI.BigInt('128');
 let num2 = new bI.BigInt('25381521125');
+
+console.log(`Num1 = ${num1}\nNum2 = ${num2}`);
 
 console.log('No. of digits: ' + num1.totalNoOfDigits());
 
@@ -50,8 +52,38 @@ console.time('equal');
 console.log('num1 == num2: ' + num1.equal(num2));
 console.timeEnd('equal');
 
+function factorial2(num1) {
+    let temp = num1.abs();
+    let temp2 = new bI.BigInt('1');
+    let zerosObject = new bI.BigInt('0');
+    let onesObject = new bI.BigInt('1');
+    let n = parseInt(temp.toString());
+    let originalNumber = n;
+    console.log('Time Factorial function takes for the first 50 numbers');
+
+    while (!temp.equal(zerosObject)) {
+        if (n % 50 === 0 && n !== originalNumber) {
+            console.time('50numbers');
+        }
+        
+        temp2 = temp.multiply(temp2);
+        if (n % 50 === 0 && n !== originalNumber) {
+            console.timeEnd('50numbers');
+        }
+        temp = temp.subtract(onesObject);
+        n--;
+    }
+
+    return temp2;
+}
+
+console.time('Factorial');
+console.log("Factorial of num1: " + factorial(num1).toString());
+console.timeEnd('Factorial');
+
 // Factorial Compute
 function factorial(num1) {
+
     if (num1.equal(new bI.BigInt('1'))) {
         return new bI.BigInt('1');
     }
@@ -60,9 +92,9 @@ function factorial(num1) {
 }
 
 
-console.time('factorial');
-console.log('Factorial of num1: ' + factorial(num1));
-console.timeEnd('factorial');
+// console.time('factorial2');
+// console.log('Factorial of num1: ' + factorial2(num1));
+// console.timeEnd('factorial2');
 
 
 
