77c6afc5e1f777fb73f85a786aaf18c5f71c70b0
Suraj Pulloor <thegamer600@gmail.com>
2019-07-26 02:20:49 +0530

    Refactored the tests into a single module, created test groups

---
+++ test/bigint.test.js
@@ -0,0 +1,266 @@
+const BigInt = require('../dist/bigint');
+
+// Group 1
+describe('num1 === num2', () => {
+
+    // test variables
+    let num1 = new BigInt('2413641');
+    let num2 = new BigInt('2413641');
+
+    // tests
+    // Test #1
+    it('Adding num1 + num2', () => {
+        expect(num1.add(num2).toString()).toBe('4827282');
+    });
+
+    // Test #2
+    it('Subtracting num1 - num2', () => {
+        expect(num1.subtract(num2).toString()).toBe('0');
+    });
+
+    // Test #3
+    it('Reverse Subtracting num2 - num1', () => {
+        expect(num2.subtract(num1).toString()).toBe('0');
+    });
+
+    // Test #4
+    it('Multiplying num1 * num2', () => {
+        expect(num1.multiply(num2).toString()).toBe('5825662876881');
+    });
+
+    // Test #5
+    it('LessThan num1 < num2', () => {
+        expect(num1.lessThan(num2)).toBe(false);
+    });
+
+    // Test #6
+    it('LessThanEqual num1 <= num2', () => {
+        expect(num1.lessThanEqual(num2)).toBe(true);
+    });
+
+    // Test #7
+    it('GreaterThan num1 > num2', () => {
+        expect(num1.greaterThan(num2)).toBe(false);
+    });
+
+    // Test #8
+    it('GreaterThanEqual num1 >= num2', () => {
+        expect(num1.greaterThanEqual(num2)).toBe(true);
+    });
+
+    // Test #9
+    it('Equal num1 == num2', () => {
+        expect(num1.equal(num2)).toBe(true);
+    });
+});
+
+// Group 2
+describe('num1 < 0 && num2 < 0', () => {
+    
+    let num1 = new bI.BigInt('-2552359');
+    let num2 = new bI.BigInt('-2538458');
+
+    // Test #1
+    it('Adding num1 + num2', () => {
+        expect(num1.add(num2).toString()).toBe('-5090817');
+    });
+
+    // it #2
+    it('Subtracting num1 - num2', () => {
+        expect(num1.subtract(num2).toString()).toBe('-13901');
+    });
+
+    // it #3
+    it('Reverse Subtracting num2 - num1', () => {
+        expect(num2.subtract(num1).toString()).toBe('13901');
+    });
+
+    // it #4
+    it('Multiplying num1 * num2', () => {
+        expect(num1.multiply(num2).toString()).toBe('6479056122422');
+    });
+
+    // it #5
+    it('LessThan num1 < num2', () => {
+        expect(num1.lessThan(num2)).toBe(true);
+    });
+
+    // it #6
+    it('LessThanEqual num1 <= num2', () => {
+        expect(num1.lessThanEqual(num2)).toBe(true);
+    });
+
+    // it #7
+    it('GreaterThan num1 > num2', () => {
+        expect(num1.greaterThan(num2)).toBe(false);
+    });
+
+    // it #8
+    it('GreaterThanEqual num1 >= num2', () => {
+        expect(num1.greaterThanEqual(num2)).toBe(false);
+    });
+
+    // it #9
+    it('Equal num1 == num2', () => {
+        expect(num1.equal(num2)).toBe(false);
+    });
+});
+
+describe('num1 < 0 && num2 > 0', () => {
+    
+    // test variables
+    let num1 = new bI.BigInt('-2552359');
+    let num2 = new bI.BigInt('2538458');
+
+
+    // Test #1
+    it('Adding num1 + num2', () => {
+        expect(num1.add(num2).toString()).toBe('-13901');
+    });
+
+    // it #2
+    it('Subtracting num1 - num2', () => {
+        expect(num1.subtract(num2).toString()).toBe('-5090817');
+    });
+
+    // it #3
+    it('Reverse Subtracting num2 - num1', () => {
+        expect(num2.subtract(num1).toString()).toBe('5090817');
+    });
+
+    // it #4
+    it('Multiplying num1 * num2', () => {
+        expect(num1.multiply(num2).toString()).toBe('-6479056122422');
+    });
+
+    // it #5
+    it('LessThan num1 < num2', () => {
+        expect(num1.lessThan(num2)).toBe(true);
+    });
+
+    // it #6
+    it('LessThanEqual num1 <= num2', () => {
+        expect(num1.lessThanEqual(num2)).toBe(true);
+    });
+
+    // it #7
+    it('GreaterThan num1 > num2', () => {
+        expect(num1.greaterThan(num2)).toBe(false);
+    });
+
+    // it #8
+    it('GreaterThanEqual num1 >= num2', () => {
+        expect(num1.greaterThanEqual(num2)).toBe(false);
+    });
+
+    // it #9
+    it('Equal num1 == num2', () => {
+        expect(num1.equal(num2)).toBe(false);
+    });
+});
+
+describe('num1 > 0 && num2 < 0', () => {
+    // test variables
+    let num1 = new bI.BigInt('2552359');
+    let num2 = new bI.BigInt('-2538458');
+
+
+    // Test #1
+    it('Adding num1 + num2', () => {
+        expect(num1.add(num2).toString()).toBe('13901');
+    });
+
+    // it #2
+    it('Subtracting num1 - num2', () => {
+        expect(num1.subtract(num2).toString()).toBe('5090817');
+    });
+
+    // it #3
+    it('Reverse Subtracting num2 - num1', () => {
+        expect(num2.subtract(num1).toString()).toBe('-5090817');
+    });
+
+    // it #4
+    it('Multiplying num1 * num2', () => {
+        expect(num1.multiply(num2).toString()).toBe('-6479056122422');
+    });
+
+    // it #5
+    it('LessThan num1 < num2', () => {
+        expect(num1.lessThan(num2)).toBe(false);
+    });
+
+    // it #6
+    it('LessThanEqual num1 <= num2', () => {
+        expect(num1.lessThanEqual(num2)).toBe(false);
+    });
+
+    // it #7
+    it('GreaterThan num1 > num2', () => {
+        expect(num1.greaterThan(num2)).toBe(true);
+    });
+
+    // it #8
+    it('GreaterThanEqual num1 >= num2', () => {
+        expect(num1.greaterThanEqual(num2)).toBe(true);
+    });
+
+    // it #9
+    it('Equal num1 == num2', () => {
+        expect(num1.equal(num2)).toBe(false);
+    });
+});
+
+describe('num1 > 0 && num2 > 0', () => {
+    
+    // test variables
+    let num1 = new bI.BigInt('2552359');
+    let num2 = new bI.BigInt('2538458');
+
+
+    // Test #1
+    it('Adding num1 + num2', () => {
+        expect(num1.add(num2).toString()).toBe('5090817');
+    });
+
+    // it #2
+    it('Subtracting num1 - num2', () => {
+        expect(num1.subtract(num2).toString()).toBe('13901');
+    });
+
+    // it #3
+    it('Reverse Subtracting num2 - num1', () => {
+        expect(num2.subtract(num1).toString()).toBe('-13901');
+    });
+
+    // it #4
+    it('Multiplying num1 * num2', () => {
+        expect(num1.multiply(num2).toString()).toBe('6479056122422');
+    });
+
+
+    // it #5
+    it('LessThan num1 < num2', () => {
+        expect(num1.lessThan(num2)).toBe(false);
+    });
+
+    // it #6
+    it('LessThanEqual num1 <= num2', () => {
+        expect(num1.lessThanEqual(num2)).toBe(false);
+    });
+
+    // it #7
+    it('GreaterThan num1 > num2', () => {
+        expect(num1.greaterThan(num2)).toBe(true);
+    });
+
+    // it #8
+    it('GreaterThanEqual num1 >= num2', () => {
+        expect(num1.greaterThanEqual(num2)).toBe(true);
+    });
+
+    // it #9
+    it('Equal num1 == num2', () => {
+        expect(num1.equal(num2)).toBe(false);
+    });
+});
